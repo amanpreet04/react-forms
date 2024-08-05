08ea670171e1f0ce043a74b4caeae8a6ab8fb73a
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-05 22:30:05 +0530

    Added validations

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
     const fd = new FormData(event.target);
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data["confirm-password"]) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
   }
 
   return (
@@ -36,6 +44,9 @@ export default function Signup() {
             type="password"
             name="confirm-password"
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords are not matching</p>}
+          </div>
         </div>
       </div>
 
